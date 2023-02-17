c2889cfc2463f2e72d1aa96c6075f26d7a5a8f4c
Mercia Keila <merciakeiladev@gmail.com>
2023-02-17 10:51:27 -0300

    fix: Removendo alguns itens da navigation

--- src/navigation/LinkingConfiguration.ts
+++ src/navigation/LinkingConfiguration.ts
@@ -1,37 +1,17 @@
-import { LinkingOptions } from '@react-navigation/native';
-import * as Linking from 'expo-linking';
+import { LinkingOptions } from "@react-navigation/native";
+import * as Linking from "expo-linking";
 
-import { RootStackParamList } from 'app/interfaces/types';
+import { RootStackParamList } from "app/interfaces/types";
 
 const linking: LinkingOptions<RootStackParamList> = {
-  prefixes: [Linking.createURL('/')],
+  prefixes: [Linking.createURL("/")],
   config: {
     screens: {
       Root: {
         screens: {
           Home: {
             screens: {
-              HomeScreen: 'home',
-            },
-          },
-          Repair: {
-            screens: {
-              RepairScreen: 'repair',
-            },
-          },
-          Shop: {
-            screens: {
-              ShopScreen: 'shop',
-            },
-          },
-          History: {
-            screens: {
-              HistoryScreen: 'history',
-            },
-          },
-          Settings: {
-            screens: {
-              SettingsScreen: 'settings',
+              HomeScreen: "home",
             },
           },
         },
